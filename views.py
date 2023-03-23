2059180ae6a09679e52dfe77abb78039b6eddd68
corn-mendoza <26883996+corn-mendoza@users.noreply.github.com>
2023-03-23 08:12:18 -0500

    updated documentaion in views

--- views.py
+++ views.py
@@ -3,15 +3,18 @@ from flask import Blueprint, render_template, request, jsonify, redirect, url_fo
 
 views = Blueprint(__name__, "views")
 
+# default route
 @views.route("/")
 def home():
     return render_template("index.html", value1 = "Corn")
 
+# sample for taking a username as part of url
 @views.route("/profile/<username>")
 def profile(username):
     return render_template("index.html", value1 = username)
 
-@views.route("/profile2")
+# sample page to take arguments
+@views.route("/getprofile")
 def profile2():
     args = request.args
     name = args.get("name")
@@ -22,16 +25,18 @@ def profile2():
 def profile3():
     return render_template("profile.html")
 
+# sample to take json data as parameters
 @views.route("/json")
 def get_json():
     return jsonify({'name':'corn', 'value2':10})
 
+# sample to return json data
 @views.route("/data")
 def get_data():
     data = request.json
     return jsonify(data)
 
-# redirect
+# sample redirect
 @views.route("/goto")
 def goto():
     return redirect(url_for("views.home"))
